5c378d5f745b0d7ae802d99f2c3fe053d58345e7
Mahir Molai <mahir@molai.dev>
2022-05-06 16:48:08 +0500

    Get rid of restart message

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,4 @@
 const discord = require('discord.js');
-
-const { MessageEmbed } = require('discord.js');
-const e = require('../utils/embeds.json');
 module.exports = {
   name: "ready",
   once: true,
@@ -10,16 +7,6 @@ module.exports = {
    * @param {discord.Client} client 
    */
   async execute(client) {
-    const botNews = client.channels.cache.find(ch => ch.name === 'bot-news');
-    botNews.send({ embeds: [embed] })
-
-    const embed = new MessageEmbed()
-		.setTitle("Bot Started")
-		.setDescription(`${client.user.tag} is now online!`)
-		.setColor(e.color)
-		.setTimestamp()
-		.setFooter(e.footer);
-
     console.log("Logged in.");
     client.user.setActivity('with Your Mom')
   },
